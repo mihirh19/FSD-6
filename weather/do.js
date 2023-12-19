188e79049ce1ebb6961dbbccf48944ae91bbf3e7
mihirh19 <miheerhadvani990@gmail.com>
2023-12-19 17:39:33 +0530

    Weather api fetch

---
+++ weather/do.js
@@ -0,0 +1,36 @@
+async function search() {
+   ou = document.getElementById("out");
+
+   inp = document.getElementById("inp");
+   ou.innerText = ""
+   data = null
+
+   // var xhr = new XMLHttpRequest();
+
+
+   // xhr.open("GET", "https://api.api-ninjas.com/v1/weather?city=" + inp.value, true)
+   // xhr.setRequestHeader("X-Api-Key", "U7UqiXUupn4BmFN7sykIiw==vx7VfSZBWKVTjclj")
+   // xhr.onreadystatechange = function () {
+   // if (xhr.readyState == 4 && xhr.status == 200) {
+   //  data = JSON.parse(xhr.responseText)
+   // console.log(data["temp"])
+   // ou.innerText = data["temp"]
+   // }
+   // }
+
+   // xhr.send(); 
+
+   await fetch("https://api.api-ninjas.com/v1/weather?city=" + inp.value, {
+      method: "GET",
+      headers: {
+         "X-Api-Key": "U7UqiXUupn4BmFN7sykIiw==vx7VfSZBWKVTjclj",
+      }
+   }).then(response => response.json()).then(data => {
+      console.log(data["temp"])
+      ou.innerText = "Temp  : " + data["temp"] + "°C" + "\n" + "Humidity  : " + data["humidity"] + "%" + "\n" + "Wind Speed  : " + data["wind_speed"] + "km/h"
+   }).catch(err => {
+      console.log(err)
+      ou.innerText = "City not found"
+   })
+
+}
