ec3e4860fb2494c20f85d0afd9b6b8b71d5070f7
mihirh19 <miheerhadvani990@gmail.com>
2023-12-19 08:30:36 +0530

    added script file

--- dom/script.js
+++ dom/script.js
@@ -1,11 +1,26 @@
+
 function submit() {
    console.log("submit");
-   let ullist = document.getElementById("stulist");
+   const ullist = document.getElementById("stulist");
    let lilist = document.createElement("li");
    n = document.getElementById("name").value
    email = document.getElementById("email").value
    age = document.getElementById("age").value
-   lilist.textContent = n + "  " + email + "  " + age;
+   divname = document.createElement("div");
+   divemail = document.createElement("div");
+   divage = document.createElement("div");
+   divname.setAttribute("class", "namehi");
+   divemail.setAttribute("class", "emailhi");
+   divage.setAttribute("class", "agehi");
+   divname.textContent = n;
+   divemail.textContent = email;
+   divage.textContent = age;
+   lilist.appendChild(divname);
+   lilist.appendChild(divemail);
+   lilist.appendChild(divage);
+
+   //lilist.innerHTML = "<div class='namehi'>" + n + "</div> " + "<div class='emailhi'>" + email + "</div> " + "<div class='agehi'>" + age + "</div> ";
+   // lilist.textContent = n + "  " + email + "  " + age;
 
    let removebtn = document.createElement("button");
    removebtn.textContent = "remove";
@@ -14,4 +29,27 @@ function submit() {
    }
    lilist.appendChild(removebtn);
 
+   ullist.appendChild(lilist);
+
+}
+
+function namehi() {
+   const namehiele = document.getElementsByClassName("namehi");
+   for (n of namehiele) {
+      n.style.color = "red"
+   }
+}
+
+function emailhi() {
+   const emailhiele = document.getElementsByClassName("emailhi");
+   for (n of emailhiele) {
+      n.style.color = "blue"
+   }
+}
+
+function agehi() {
+   const agehiele = document.getElementsByClassName("agehi");
+   for (n of agehiele) {
+      n.style.color = "orange"
+   }
 }
